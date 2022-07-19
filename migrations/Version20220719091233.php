6eb6f05c07e602683a0315191c661398d453b001
Евгений Комаров <komarov.j@gmail.com>
2022-07-19 13:49:14 +0300

    Условий выборки данных дополнено обязательным наличием координат

--- migrations/Version20220719091233.php
+++ migrations/Version20220719091233.php
@@ -17,8 +17,7 @@ final class Version20220719091233 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql(
-            '
-            create or replace view map_element as
+            'create or replace view map_element as
                 select s.summary_id as id,
                      s.accident_type as type,
                      s.accident_memo as memo,
@@ -28,7 +27,9 @@ final class Version20220719091233 extends AbstractMigration
                 from summary s
                          left join address a on s.accident_address = a.address_id
                          left join place p on p.fias_guid = a.fias_guid
-                where a.house = 1'
+                where a.house = 1
+                    and p.LATITUDE is not null
+                    and p.LONGITUDE is not null'
         );
     }
 
